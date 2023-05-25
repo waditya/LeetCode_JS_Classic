f2f6466d64434f0bf6fb7a255a6db4f2d5089873
waditya <waditya02@gmail.com>
2023-05-24 22:25:34 -0500

    Solution for LC242_isAnagram

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,38 @@
-function isAnagram(s, t) {}
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length != t.length){
+        return false;
+    }else {
+        let set2 = new Set(t);
+        // console.log(set1);
+        // console.log(set2);
+        
+        const sCharCounts = {}; // anagram {A: 3 , N: 1, G :1, R:1, M:1}
+
+        // Fill sCharCounts 
+
+        for (let i=0; i < s.length; i++){
+            const sChar = s[i];
+            sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1
+        }
+
+        for (let i=0; i <t.length;i++){
+            const tChar = t[i];
+
+            if (!sCharCounts[tChar]){
+                return false;
+            } else {
+                sCharCounts[tChar] --;
+            }
+        }
+
+    }
+    return true;
+}
+
 
 module.exports = isAnagram;
