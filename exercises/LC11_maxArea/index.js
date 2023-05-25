a9f275ae7428c914b2d7cddb418466b3e49035f1
waditya <waditya02@gmail.com>
2023-05-25 12:08:41 -0500

    Reduce time code for LC11 with O[n] time complexity

--- exercises/LC11_maxArea/index.js
+++ exercises/LC11_maxArea/index.js
@@ -1,20 +1,21 @@
 function maxArea(height) {
 
-    console.log( "Height array is : "+height);
+   // console.log( "Height array is : "+height);
    let leftPtr=0;
    let rightPtr=height.length-1;
-   let minHeight = height[leftPtr] > height[rightPtr]?height[rightPtr]: height[leftPtr];
+   // let minHeight = height[leftPtr] > height[rightPtr]?height[rightPtr]: height[leftPtr];
+   let minHeight = Math.min(height[leftPtr], height[rightPtr]);
 
    let area=(rightPtr-leftPtr)* minHeight;
 
    while(rightPtr > leftPtr){
        if(height[leftPtr] < height[rightPtr]){
            leftPtr++;
-           minHeight = height[leftPtr] > height[rightPtr]?height[rightPtr]: height[leftPtr];
+           minHeight = Math.min(height[leftPtr], height[rightPtr]);
        } else
        {
            rightPtr--;
-           minHeight = height[leftPtr] > height[rightPtr]?height[rightPtr]: height[leftPtr];
+           minHeight = Math.min(height[leftPtr], height[rightPtr]);
        }
        calculatedarea= (rightPtr-leftPtr)* minHeight;
        area=area>calculatedarea?area:calculatedarea;
