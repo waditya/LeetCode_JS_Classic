e8b8087459036f981184d7fd9067c5c4297d1119
waditya <waditya02@gmail.com>
2023-05-25 12:02:02 -0500

    Solution for LC11 by using leftPtr and rightPtr with O[n] complexity

--- exercises/LC11_maxArea/index.js
+++ exercises/LC11_maxArea/index.js
@@ -1,3 +1,26 @@
-function maxArea(height) {}
+function maxArea(height) {
+
+    console.log( "Height array is : "+height);
+   let leftPtr=0;
+   let rightPtr=height.length-1;
+   let minHeight = height[leftPtr] > height[rightPtr]?height[rightPtr]: height[leftPtr];
+
+   let area=(rightPtr-leftPtr)* minHeight;
+
+   while(rightPtr > leftPtr){
+       if(height[leftPtr] < height[rightPtr]){
+           leftPtr++;
+           minHeight = height[leftPtr] > height[rightPtr]?height[rightPtr]: height[leftPtr];
+       } else
+       {
+           rightPtr--;
+           minHeight = height[leftPtr] > height[rightPtr]?height[rightPtr]: height[leftPtr];
+       }
+       calculatedarea= (rightPtr-leftPtr)* minHeight;
+       area=area>calculatedarea?area:calculatedarea;
+   }
+
+   return area;
+}
 
 module.exports = maxArea;
