aa42636d26f8f6ee3f3b0cacd58ea9f1496760bc
waditya <waditya02@gmail.com>
2023-05-25 10:19:35 -0500

    Adding O[n] and O[n^2] solutions for LC238 Product Except self

--- exercises/LC238_productExceptSelf/index.js
+++ exercises/LC238_productExceptSelf/index.js
@@ -1,3 +1,65 @@
-const productExceptSelf = nums => {};
+const productExceptSelf = nums => {
+   
+   // Iterate of existing array to get a unit array 
+
+   let unitarr = nums.map( (n) => {
+    // For each element in the array return 1 as the output value
+    return 1;
+   });
+
+   let product = 1;
+
+   // Multiply from left to right 
+
+   for(let ctr=0; ctr < nums.length; ctr++){
+    unitarr[ctr] = unitarr[ctr] * product;
+    product = product * nums[ctr];
+   }
+   
+   product =1;
+
+   // Multiply from right 
+
+   for (let index = nums.length-1; index >=0; index--){
+    unitarr[index] = unitarr[index] * product;
+    product = product * nums[index];
+   }
+
+   return unitarr;
+
+   /* Below is O[n^2] solution 
+    before_arr= [];
+    after_arr = [];
+    product_arr = [];
+
+    before_arr[0]=1;
+    after_arr[nums.length-1] = 1;
+    initialValue=1;
+    for(let i=1; i < nums.length; i++){
+         temparr=nums.slice(0,i);
+
+         before_arr[i]=temparr.reduce((result, item) => {
+             return result*item;
+        }, initialValue);
+    }
+
+    for(let j=0; j < nums.length-1; j++){
+        
+        temparr2 = nums.slice(j+1, nums.length);
+        after_arr[j]= temparr2.reduce((result02, item02) => {
+            return result02*item02;
+        }, initialValue);
+    }
+    // console.log("Before Array is : "+before_arr);
+    // console.log("After Array is : "+after_arr);
+
+    for (let k=0; k < nums.length; k++){
+        product_arr[k] = before_arr[k] * after_arr[k];
+    }
+    
+    // console.log("After Array is : "+product_arr);
+    return product_arr;
+    */
+};
 
 module.exports = productExceptSelf;
