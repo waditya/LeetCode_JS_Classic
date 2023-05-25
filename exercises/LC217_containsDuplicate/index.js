c3c72df88e88e879249e75e4a87720b75e748308
waditya <waditya02@gmail.com>
2023-05-25 10:32:32 -0500

    Adding solution for LC217 Check if array contains duplicates

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,16 @@
-const containsDuplicate = nums => {};
+const containsDuplicate = nums => {
+
+    // console.log("Array length is : "+nums.length);
+
+    // Create a Set using array
+
+    let set = new Set(nums);
+    // console.log("Size of Set is : "+set.size);
+
+    return set.size===nums.length?false:true;
+};
+
 
 module.exports = containsDuplicate;
+
+
