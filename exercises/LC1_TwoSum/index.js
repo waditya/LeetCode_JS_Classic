a2843f86252860075f9b9a0c05bdd86be49a696b
waditya <waditya02@gmail.com>
2023-05-28 14:22:02 -0500

    LC1_TwoSum solution with O[n] complexity

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -6,6 +6,19 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,5,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+function twoSum(arr, target) {const map1 = new Map();
+    let sol_arr = []
+ 
+    for(let i=0; i< arr.length; i++){
+        if(!map1.has(target-arr[i])){
+            map1.set(arr[i], target-arr[i]);
+        } else {
+            sol_arr.push(i);
+            sol_arr.push(arr.indexOf(target-arr[i]));
+            return sol_arr;
+        }
+        
+    }
+    }
 
 module.exports = twoSum;
