18daefe84c1d71bc3cabeaca0b967bc213466fda
waditya <waditya02@gmail.com>
2023-05-27 23:14:09 -0500

    Updated comments for the algo LC 121 solution

--- exercises/LC121_maxProfit/index.js
+++ exercises/LC121_maxProfit/index.js
@@ -2,6 +2,12 @@ var maxProfit = function(prices) {
  let maxProfit = 0;
  let cheapestPrice = prices[0];
 
+ // We buy before we sell. Iterater from left to right
+ // While iterating, look for the lowest number. Replace it with a new number if corresponding 
+ // array element is lower than current "lowest element"
+ // Subtract lowest element with current value to get current profit
+ // Store and/or replace maxProfit if current profit more than existing maxProfit.
+ 
  for (let i =0; i < prices.length; i++){
     const price = prices[i];
 
